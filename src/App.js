e11ce4f7f912bdfc1dcbeb2e7b0f7dfb385d3b78
cocodori <moradulee@gmail.com>
2023-01-09 21:31:48 +0900

    add list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@
  import "./App.css";
 
  export default class App extends Component {
+    btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cusor: "pointer",
+        float: "right"
+    };
+
+    getStyle = () => {
+       return {
+        padding: "10px",
+        borderBottom: "1px #ccc dotted",
+        textDecoration: "none"
+       } 
+    }
+    
     render() {
         return(
             <div className="container">
                 <div className="todoBlock">
                     <div className="title">
                         <h1>Todo</h1>
+                    </div>
+
+                    <div style={this.getStyle()}>
+                        <input type="checkbox" defaultChecked={false} />
+                        리액트 공부
+                        <button style={this.btnStyle}>x</button>
+                    </div>
+
+                    <div style={this.getStyle()}>
+                        <input type="checkbox" defaultChecked={false} />
+                        Typescript 공부
+                        <button style={this.btnStyle}>x</button>
                     </div>
                 </div>
             </div>
