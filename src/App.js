d5eb925a2b3b0f539a168c5f02e32eea309bb529
cocodori <moradulee@gmail.com>
2023-01-11 20:40:29 +0900

    remove list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
- import React, {Component} from "react"
- import "./App.css";
+import React, { Component } from "react"
+import "./App.css";
 
- export default class App extends Component {
+export default class App extends Component {
     btnStyle = {
         color: "#fff",
         border: "none",
@@ -12,34 +12,49 @@
     };
 
     getStyle = () => {
-       return {
-        padding: "10px",
-        borderBottom: "1px #ccc dotted",
-        textDecoration: "none"
-       } 
+        return {
+            padding: "10px",
+            borderBottom: "1px #ccc dotted",
+            textDecoration: "none"
+        }
     }
-    
+
+    state = {
+        todoData: [
+            {
+                id: "1",
+                title: "react study",
+                completed: true
+            },
+            {
+                id: "2",
+                title: "typescript study",
+                completed: false
+            }
+        ]
+    }
+
+    handleClick = (id) => {
+        let newTodoData = this.state.todoData.filter(data => data.id !== id);
+        this.setState({todoData: newTodoData});
+    }
+
     render() {
-        return(
+        return (
             <div className="container">
                 <div className="todoBlock">
                     <div className="title">
                         <h1>Todo</h1>
                     </div>
-
-                    <div style={this.getStyle()}>
-                        <input type="checkbox" defaultChecked={false} />
-                        리액트 공부
-                        <button style={this.btnStyle}>x</button>
-                    </div>
-
-                    <div style={this.getStyle()}>
-                        <input type="checkbox" defaultChecked={false} />
-                        Typescript 공부
-                        <button style={this.btnStyle}>x</button>
-                    </div>
+                    {this.state.todoData.map(data => (
+                        <div style={this.getStyle()} key={data.id}>
+                            <input type="checkbox" defaultChecked={false} />
+                            {data.title}
+                            <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+                        </div>
+                    ))}
                 </div>
             </div>
         )
     }
- }
+}
