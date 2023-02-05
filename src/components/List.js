4aea470e76760443b997681e158e583a80433349
cocodori <moradulee@gmail.com>
2023-02-05 16:37:17 +0900

    Lists.js, List.js 컴포넌트 분리

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,8 @@
-import { root } from 'postcss';
-import React from 'react'
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import React from "react";
 
-export default function List ({todoData, setTodoData}) {
-
+const List = ({
+    id, title, completed, todoData, setTodoData, provided, snapshot
+}) => {
     const handleCompleteChange = (id) => {
         let newTodoData = todoData.map(data => {
             if (data.id === id) {
@@ -22,58 +21,29 @@ export default function List ({todoData, setTodoData}) {
         setTodoData(newTodoData);
     }
 
-    const handleEnd = (result) => {
-        if (!result.destination) return
-
-        const newTodoData = [...todoData];
-
-        //1. 변경하는 아이템을 배열에서 지운다.
-        //2. return 값으로 지워진 아이템을 잡아줌
-        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
-
-        // 원하는 자리에 reorderedItem을 insert 한다.
-        newTodoData.splice(result.destination.index, 0, reorderedItem);
-        setTodoData(newTodoData)
-    }
-
     return (
-        <div>
-            <DragDropContext onDragEnd={handleEnd}>
-                <Droppable droppableId="todo">
-                    {(provided) => (
-                        <div {...provided.droppableProps} ref={provided.innerRef}>
-                            {todoData.map((data, index) => (
-                                <Draggable
-                                    key={data.id}
-                                    draggableId={data.id.toString()}
-                                    index={index}
-                                >
-                                    {(provided, snapshot) => (
-                                        <div 
-                                            key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
-                                            className={`${snapshot.isDragging ? "bg-gray-400": "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}>
-                                            <div className="items-center"> 
-                                                <input 
-                                                    type="checkbox" 
-                                                    defaultChecked={false} 
-                                                    onChange={() => handleCompleteChange(data.id)} 
-                                                />{" "}
-                                                <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
-                                            </div>
-                                            <div>
-                                                <button className="px-4 py-2 float-right " onClick={() => handleClick(data.id)}>
-                                                    x
-                                                </button>
-                                            </div>
-                                        </div>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {provided.placeholder}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext>
+        <div
+            key={id} 
+            {...provided.draggableProps} 
+            ref={provided.innerRef} 
+            {...provided.dragHandleProps}
+            className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
+        >
+            <div className="items-center">
+                <input
+                    type="checkbox"
+                    defaultChecked={false}
+                    onChange={() => handleCompleteChange(id)}
+                />{" "}
+                <span className={completed ? "line-through" : undefined}>{title}</span>
+            </div>
+            <div>
+                <button className="px-4 py-2 float-right " onClick={() => handleClick(id)}>
+                    x
+                </button>
+            </div>
         </div>
     )
 }
+
+export default List
