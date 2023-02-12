799a9e7912832b91a13372a940b26c8bb16473ff
cocodori <moradulee@gmail.com>
2023-02-12 16:13:21 +0900

    수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -27,13 +27,18 @@ export default function App() {
     const handleClick = useCallback((id) => {
         let newTodoData = todoData.filter(data => data.id !== id);
         setTodoData(newTodoData);
-    }, [todoData]) 
+    }, [todoData]);
+
+    const handleRemoveClick = () => {
+        setTodoData([]);
+    }
 
     return (
         <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
             <div className="w-full p-6 m-4 bg-white rounded shadow log:w-3/4 lg:max-w-lg">
                 <div className="flex justify-between mb-3">
                     <h1>할 일</h1>
+                    <button onClick={handleRemoveClick}>delete all</button>
                 </div>
 
                 <List handleClick={handleClick} todoData={todoData} setTodoData={setTodoData}/>
